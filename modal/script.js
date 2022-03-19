64a7d83c368c7426927e1bb3da912d247a5a1232
Vladimir Belebnev <94782107+VladimirBelebnev@users.noreply.github.com>
2022-03-19 14:00:09 +0300

    update

--- modal/script.js
+++ modal/script.js
@@ -1,45 +1,38 @@
-// Создаем переменные ([data-modal] – кнопка вызова)
 const modalTrigger = document.querySelectorAll('[data-modal]'),
     modal = document.querySelector('.modal');
 
-// Функция открытия модального окна
 function openModal() {
     modal.classList.add('show');
     modal.classList.remove('hide');
-    document.body.style.overflow = 'hidden'; // Чтобы окно не пролистовалось
-    clearInterval(modalTimerId); // Открытие модального окна через время
+    document.body.style.overflow = 'hidden'; 
+    clearInterval(modalTimerId); 
 }
 
-// Вешаем на кнопки обработчик события
 modalTrigger.forEach((btn) => {
     btn.addEventListener('click', openModal);
 });
 
-// Функция закрытия модального окна
+
 function closeModal() {
     modal.classList.add('hide');
     modal.classList.remove('show');
-    document.body.style.overflow = ''; // Убираем
+    document.body.style.overflow = '';
 }
 
-// Закрытия окна если кликнуть вне модального окна
 modal.addEventListener('click', (event) => {
     if (event.target == modal || event.target.getAttribute('data-close') == '') {
         closeModal();
     }
 });
 
-// Закрытие модального окна по клавише  esc 
 document.addEventListener('keydown', (event) => {
     if (event.code === 'Escape' && modal.classList.contains('show')) {
         closeModal();
     }
 });
 
-// Открытие модального окна по времени
 const modalTimerId = setTimeout(openModal, 50000);
 
-// Открытие модального окна, если пользователь долистал до конца страницы
 function showModalByScroll() {
     if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
         openModal();
