e2ec0ac9a42d1c07ae63e1652a01910ade2986fc
Vladimir Belebnev <94782107+VladimirBelebnev@users.noreply.github.com>
2022-03-19 13:56:56 +0300

    update

--- easy-slider/script.js
+++ easy-slider/script.js
@@ -1,4 +1,3 @@
-// Создаем переменную
 const slides = document.querySelectorAll('.offer__slide'),
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('.offer__slider-next'),
@@ -8,32 +7,25 @@ let slideIndex = 1; // Изначальное значение, индекс
 
 showSlides(slideIndex);
 
-// Проерка на 0 total
 if (slides.length < 10) {
     total.textContent = `0${slides.length}`;
 } else {
     total.textContent = slides.length;
 }
 
-// Функция показа слайдера
 function showSlides(n) {
-    // Если больше чем колличество слайдов
     if (n > slides.length) {
         slideIndex = 1;
     }
 
-    // Если меньше чем колличество слайдов
     if (n < 1) {
         slideIndex = 4;
     }
 
-    // Скрываем слайдры
     slides.forEach(item => item.style.display = 'none');
 
-    // Показываем первый слайд
     slides[slideIndex - 1].style.display = 'block';
 
-    // Проверяем на 0 current
     if (slides.length < 10) {
         current.textContent = `0${slideIndex}`;
     } else {
@@ -41,17 +33,14 @@ function showSlides(n) {
     }
 }
 
-// Функция переключения слайдов
 function plusSlides(n) {
     showSlides(slideIndex +=n);
 }
 
-// Стрелка назад
 prev.addEventListener('click', () => {
     plusSlides(-1);
 });
 
-// Стрелка вперед
 next.addEventListener('click', () => {
     plusSlides(+1);
 });
