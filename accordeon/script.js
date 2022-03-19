de96e770bc0aa3c07588c444b085dfa851a6793d
Vladimir Belebnev <94782107+VladimirBelebnev@users.noreply.github.com>
2022-03-19 21:18:24 +0300

    add accordion

---
+++ accordeon/script.js
@@ -0,0 +1,20 @@
+const accordeon = () => {
+    const accordeon = document.querySelector('.feature-list'),
+        accordeonBtn = accordeon.querySelectorAll('.feature__link');
+
+    accordeonBtn.forEach(item => {
+        item.addEventListener('click', () => {
+            accordeonBtn.forEach(btn => {
+                if (item !== btn) {
+                    btn.classList.remove('feature__link_active');
+                    btn.nextElementSibling.classList.add('hidden');
+                }
+            });
+
+            item.classList.toggle('feature__link_active');
+            item.nextElementSibling.classList.toggle('hidden');
+        });
+    });
+};
+
+accordeon();
