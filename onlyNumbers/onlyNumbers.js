dcf1dbb992771abb89b650d3ebd398d096d773b1
Vladimir Belebnev <94782107+VladimirBelebnev@users.noreply.github.com>
2022-04-27 18:44:39 +0300

    Delete onlyNumbers directory

--- onlyNumbers/onlyNumbers.js
+++
@@ -1,8 +0,0 @@
-const phoneInputs = document.querySelectorAll('input[name="user_phone"]');
-
-phoneInputs.forEach(item => {
-    item.addEventListener('input', () => {
-        item.value = item.value.replace(/\D/, '');
-    });
-});
-
